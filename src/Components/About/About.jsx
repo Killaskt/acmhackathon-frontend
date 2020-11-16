14b6f5eb8bdc6b1ef990b18e0d919aece7883254
Killaskt <thegimreper@gmail.com>
2020-11-16 12:31:58 -0500

    frontend finished for now

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -10,7 +10,11 @@ const About = () => {
             <Title Title={"About"}/>
             <div className="about-cont">
                 <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, necessitatibus natus deserunt eos numquam neque expedita consequatur quia ipsam, aperiam sunt dolor eum consequuntur dignissimos tempora voluptatum nulla doloribus laboriosam?
+                    Wsu nexus is the Wayne State chapter of ACM's series of events. They will mostly be hackathons but we plan to bring recruitors, presentors, and other events to help students find careers and build their resume. 
+                    <p> </p>
+                    This month we are hosting a November hackathon which will only be open to Wayne State students, as it is our first event its not at the same scale as we expect our upcoming events. 
+                    <p></p>
+                    Looking forward we plan to host another, more professional hackathon in Feburary so be sure to keep in touch!
                 </p>
             </div>
         </div>
